6aad2aa8961edeb9c0ca33fe75123b5d18533342
rayharsha <rayharsha002>
2026-03-22 13:43:14 +0530

    deleting redundent file

--- Routes/Products.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getAllProducts, getAllProductsTesting } = require("../Controllers/Products")
-router.route("/").get(getAllProducts);
-router.route("/testing").get(getAllProductsTesting);
-
-module.exports = router;
